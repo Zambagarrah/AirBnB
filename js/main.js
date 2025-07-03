6a833b346056b568797c7920482e0f3df3c8376e
lisamswt <lisanyamache05@gmail.com>
2025-07-03 19:56:57 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// - Search/filter logic
+// - Booking form validation
+// - Local storage for saving booking info
+// - Simple page routing (if using hash-based SPA)
+// - Pop-ups/modals for confirmations or login
+
+
